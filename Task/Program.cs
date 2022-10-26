e7c504b6ac9b819a118998052b2823a5be8c0c8a
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-26 10:34:50 +0300

    внесли правку в код

--- Task/Program.cs
+++ Task/Program.cs
@@ -11,14 +11,13 @@ string[] arrayStrings = new string[size];
 for (int i = 0; i < size; i++)
 {
     Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine());
-    arrayStrings[i] = element;
+    string element = Convert.ToString(Console.ReadLine()!);
+    arrayStrings[i] = element!;
 }
 
 string[] arrayFinal = new string[size];
 int len = 3;
 int pos = 0;
-
 for (int j = 0; j < size; j++)
 {
     if (arrayStrings[j].Length <= len)
